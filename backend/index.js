324406473d4af6df60b8c4681fc61381ef94bb09
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2024-09-14 12:57:53 -0300

    again production try

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,44 @@
-//entry point of backend
 import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js"
-import postRoute from "./routes/post.route.js"
-import messageRoute from "./routes/message.route.js"
-import { app,server } from "./socket/socket.js";
+import userRoute from "./routes/user.route.js";
+import postRoute from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
 import path from "path";
+ 
+dotenv.config();
 
-//env config
-dotenv.config({})
 
-//port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
-//creating a app var from express
-// const app = express();
-
 
 //middlewares
-app.use(express.json())
-app.use(cookieParser())
-app.use(urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
 const corsOptions = {
     origin: process.env.URL,
     credentials: true
 }
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-//all api's will be here->
-app.use("/api/v1/user",userRoute)
-app.use("/api/v1/post",postRoute );
-app.use("/api/v1/message",messageRoute );
+//api here
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
 
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")));
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 })
-
 
 
 server.listen(PORT, () => {
-    connectDB()
-    console.log(`Server is at port ${PORT}`)
-})
+    connectDB();
+    console.log(`Server listen at port ${PORT}`);
+});
