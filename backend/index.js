1fff6cb7cb48f95a4a2fd303db8c063aa547c156
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2024-09-14 13:02:05 -0300

    more details

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: process.env.URL,
+    origin: "http://localhost:5173",
     credentials: true
 }
 app.use(cors(corsOptions));
